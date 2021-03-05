4c54c2fbd0fad8292cf6d2894026b96a5c811108
Adenike Awofeso <adenikeawofeso@gmail.com>
2021-03-05 14:53:55 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/search?sxsrf=ALeKk0127WntZkgQ_cgzfkBi14Pwf56hmA%3A1613999734321&source=hp&ei=dq4zYJO7EMaSlwS6s7PIDg&iflsig=AINFCbYAAAAAYDO8hpyLWilzmP-G5UYYRLgA0DaGvh8g&q=Adenike&oq=Adenike&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyAgguMgIILjICCAAyAggAMgIIADICCAAyCAguEMcBEK8BMgIIADICCAA6BwgjEOoCECc6BAgAEEM6CAguELEDEIMBOgsILhCxAxCDARCRAjoFCAAQkQI6DQguELEDEMcBEKMCEEM6CgguEMcBEKMCEEM6BwguELEDEEM6BAguEEM6BwgAELEDEEM6CgguELEDEIMBEENQ6RRYkCNgiypoAXAAeACAAZgCiAGTC5IBBTAuNS4ymAEAoAEBqgEHZ3dzLXdperABCg&sclient=gws-wiz&ved=0ahUKEwiTztDGyf3uAhVGyYUKHbrZDOkQ4dUDCAc&uact=5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My name is Adenike
         </a>
       </header>
     </div>
